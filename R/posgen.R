489d707936ed571febf6b26f76b876ed3db3fbaf
Kristian Ullrich <ullrich@evolbio.mpg.de>
2018-05-03 11:33:00 +0200

    added posgen to construct sliding windows based on SNP positions as a second approach to make genome scans as compared to sliding windows based on base pair length

---
+++ R/posgen.R
@@ -0,0 +1,34 @@
+#' @title posgen
+#' @name posgen
+#' @description This function constructs a sliding window matrix based on a SNP position vector
+#' @param pos
+#' @param wlen
+#' @param start.by
+#' @param end.by
+#' @examples
+#' sw <- posgen(seq(1,975,by=5), 50, 1, 1000)
+#' @export posgen
+#' @author Kristian K Ullrich
+posgen<-function(pos,wlen=50,start.by=1,end.by=1000){
+  if(end.by<=start.by){
+    stop("end.by <= start.by")
+  }
+  start.seq<-seq(1, length(pos), by = wlen)
+  end.seq<-seq(1 + wlen - 1, length(pos), by = wlen)
+  end.seq<-c(end.seq, rep(length(pos), length(start.seq)-length(end.seq)))
+  start.seq<-pos[start.seq]
+  start.seq[1]<-start.by
+  end.seq<-pos[end.seq]
+  end.seq[length(end.seq)]<-end.by
+  start.end.matrix<-rbind(start.seq, end.seq)
+  for(i in 1:(dim(start.end.matrix)[2]-1)){
+    new.end<-floor(mean(c(start.end.matrix[2,i],start.end.matrix[1,i+1])))
+    new.start<-ceiling(mean(c(start.end.matrix[2,i],start.end.matrix[1,i+1])))
+    start.end.matrix[2,i]<-new.end
+    start.end.matrix[1,i+1]<-new.start
+  }
+  start.end.matrix<-rbind(start.end.matrix, apply(start.end.matrix, 2, function(x) {mean( c( x[1] - 1, x[2]) )} ) )
+  start.end.matrix[2,]<-as.numeric(sprintf("%.0f",start.end.matrix[2,]))
+  row.names(start.end.matrix)<-c("start", "end", "mid")
+  return(start.end.matrix)
+}
