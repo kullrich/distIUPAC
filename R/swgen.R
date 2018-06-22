faff940c0b31cdfbc3df88426181871e935ae103
kristian.ullrich <ullrich@evolbio.mpg.de>
2018-06-22 20:08:34 +0200

    #bug fix swgen

--- R/swgen.R
+++ R/swgen.R
@@ -15,8 +15,13 @@ swgen<-function(wlen=1000,wjump=100,start.by=1,end.by=1000){
     stop("end.by <= start.by")
   }
   start.seq<-seq(start.by, end.by, by = wjump)
-  end.seq<-seq(start.by + wlen - 1, end.by, by = wjump)
-  end.seq<-c(end.seq, rep(end.by, length(start.seq)-length(end.seq)))
+  if((start.by + wlen - 1)>end.by){
+    end.seq<-seq(end.by, end.by, by = wjump)
+  }
+  if((start.by + wlen - 1)<=end.by){
+    end.seq<-seq(start.by + wlen - 1, end.by, by = wjump)
+    end.seq<-c(end.seq, rep(end.by, length(start.seq)-length(end.seq)))
+  }
   start.end.matrix<-rbind(start.seq, end.seq)
   start.end.matrix<-rbind(start.end.matrix, apply(start.end.matrix, 2, function(x) {mean( c( x[1] - 1, x[2]) )} ) )
   start.end.matrix[2,]<-as.numeric(sprintf("%.0f",start.end.matrix[2,]))
